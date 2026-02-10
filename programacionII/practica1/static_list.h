0496a2e12923471e88a551bd0bca3b29d8c9be14
Miguel Taibo <dev@migueltaibo.com>
2026-02-10 11:15:36 +0100

    Definidos tanto los tipos como las funciones necesarias en static_list.h, únicamente están escritas las firmas de las funciones, todavía no están copiadas dentro de static_list.c

--- programacionII/practica1/static_list.h
+++ programacionII/practica1/static_list.h
@@ -12,6 +12,29 @@
 
 #include "types.h"
 
-/* Write your code here... */
+#define NULL 
+#define MAX 1000
+
+typedef int tItemL;
+typedef int tPosL;
+typedef struct {
+    tItemL data[MAX];
+    tPosL lastPos;
+} tList;
+
+tList createEmptyList (tList *L);
+bool isEmptyList (tList L);
+
+tPosL first (tList L);
+tPosL last (tList L);
+tPosL next (tPosL p, tList L);
+tPosL previous (tPosL p, tList L);
+
+bool insertItem (tItemL d, tPosL p, tList *L);
+tList deleteAtPosition (tPosL p, tList *L);
+tList updateItem (tItemL d, tPosL p, tList *L);
+
+tItemL getItem (tPosL, tList);
+tPosL findItem (tProjectName, tList);
 
 #endif

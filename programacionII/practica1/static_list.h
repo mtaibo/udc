8fc04e626af74facb3d08f48edfdfc549405f5fd
Miguel Taibo <dev@migueltaibo.com>
2026-02-10 11:53:53 +0100

    Corregida la firma de algunas funciones, cambiada la forma de recibir las listas, haciendolo siempre con punteros para un mejor rendimiento, además de añadida la librería stdbool para las funciones que devuelven booleanos.

--- programacionII/practica1/static_list.h
+++ programacionII/practica1/static_list.h
@@ -10,9 +10,10 @@
 #ifndef STATIC_LIST_H
 #define STATIC_LIST_H
 
+#include <stdbool.h>
 #include "types.h"
 
-#define NULL 
+#define LNULL -1
 #define MAX 1000
 
 typedef int tItemL;
@@ -25,27 +26,27 @@ typedef struct {
 // Función que crea una lista vacía, 
 // y otra función que devuelve si una lista
 // esta vacía o no
-tList createEmptyList (tList *L);
-bool isEmptyList (tList L);
+void createEmptyList (tList *L);
+bool isEmptyList (tList *L);
 
 // Funciones que sirven para 
 // moverse por la lista, obtener posiciones
 // siguientes, anteriores y primera o última posición
-tPosL first (tList L);
-tPosL last (tList L);
-tPosL next (tPosL p, tList L);
-tPosL previous (tPosL p, tList L);
+tPosL first (tList *L);
+tPosL last (tList *L);
+tPosL next (tPosL p, tList *L);
+tPosL previous (tPosL p, tList *L);
 
 // Funciones que modifican la lista, añaden, eliminan
 // o actualizan un elemento de la lista
 bool insertItem (tItemL d, tPosL p, tList *L);
-tList deleteAtPosition (tPosL p, tList *L);
-tList updateItem (tItemL d, tPosL p, tList *L);
+void deleteAtPosition (tPosL p, tList *L);
+void updateItem (tItemL d, tPosL p, tList *L);
 
 // Funciones que se encargan de devolver 
 // items o su posición en la lista
 // (no modifican la lista)
-tItemL getItem (tPosL p, tList L);
-tPosL findItem (tProjectName n, tList L);
+tItemL getItem (tPosL p, tList *L);
+tPosL findItem (tProjectName n, tList *L);
 
 #endif

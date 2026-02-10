944c4ab0e34ad42b881fb774fab5380701cf9148
Miguel Taibo <dev@migueltaibo.com>
2026-02-10 11:55:59 +0100

    Añadidas las dos funciones más básicas de las listas, crear una lista vacía y comprobar si una lista es vacía o no. Añadido también algún comentario explicando como inicializar una lista.

--- programacionII/practica1/static_list.c
+++ programacionII/practica1/static_list.c
@@ -9,4 +9,27 @@
 
 #include "static_list.h"
 
-/* Write your code here... */
+/*
+ * Esta es una forma de llamar a la función
+ * que crea las EmptyList, cuando hacemos tList lista;
+ * lo que estamos haciendo es reservar espacio en memoria
+ * para el struct tList, pero en la memoria habrá basura.
+ * Para decir que esa lista es vacía, se tendrá que declarar
+ * que la última posición de la lista es LNULL, es decir,
+ * que la lista no tiene posiciones o lo que es lo mismo,
+ * es una lista vacía.
+ *
+ * Se pasa &lista de esta forma porque la función pretende
+ * modificar el array y necesita la dirección de memoria.
+ *
+ * tList lista;
+ * createEmptyList(&lista);
+*/
+
+void createEmptyList (tList *L) {
+  L -> lastPos = LNULL; // Con esta flecha estamos haciendo (*L).lastPos
+}
+
+bool isEmptyList (tList *L) {
+  return (L -> lastPos == LNULL);
+}

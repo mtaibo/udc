9fa8295710c47b9d3d4154376bf25f7cac1e1471
Miguel Taibo <dev@migueltaibo.com>
2026-02-19 19:39:59 +0100

    Eliminados los comentarios del dynamic_list.h para no entorpecer durante el desarrollo

--- programacionII/practica1/dynamic_list.h
+++ programacionII/practica1/dynamic_list.h
@@ -22,13 +22,9 @@ typedef struct tNode {
   tPosL next;
 } tNode;
 
-// Este grupo de funciones sirven para crear una lista vacía
-// y para comprobar si una lista L está vacía o no.
 void createEmptyList (tList *L);
 bool isEmptyList (tList L);
 
-// Este grupo de funciones sirven para moverse por la lista,
-// obtener la posición siguiente, anterior, primera o última.
 tPosL first (tList L);
 tPosL last (tList L);
 tPosL next (tPosL p, tList L);
@@ -36,15 +32,10 @@ tPosL previous (tPosL p, tList L);
 
 bool createNode(tPosL *P);
 
-// Este grupo de funciones modifican la lista, añadiendo,
-// eliminando o actualizando los elementos de la misma.
 bool insertItem (tItemL d, tPosL p, tList *L);
 void deleteAtPosition (tPosL p, tList *L);
 void updateItem (tItemL d, tPosL p, tList *L);
 
-// Este grupo de funciones buscan en la lista para obtener
-// un item concreto o la posición de un ítem concreto según
-// el projectName del mismo. No modifican las listas.
 tItemL getItem (tPosL p, tList L);
 tPosL findItem (tProjectName n, tList L);
 

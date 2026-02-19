8d28f54c1547e3185d3e53a8333ba592d44c92a4
Miguel Taibo <dev@migueltaibo.com>
2026-02-19 11:31:47 +0100

    Añadida una versión provisional de dynamic_list.h e implementada una función en dynamic_list.c

--- programacionII/practica1/dynamic_list.c
+++ programacionII/practica1/dynamic_list.c
@@ -1,12 +1,33 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Miguel Taibo Pérez LOGIN 1: miguel.taibo.perez@udc.es
+ * AUTHOR 2: Marcos Estévez Besada LOGIN 2: marcos.estevez1@udc.es
+ * GROUP: 2.1
+ * DATE: ** / 02 / 26
  */
 
 #include "dynamic_list.h"
 
-/* Write your code here... */
+void createEmptyList (tList *L) {
+  *L = LNULL;
+}
+
+bool createNode(tPosL *p) {
+  *p = malloc(sizeof(struct tNode));
+  return *p != NULL;
+}
+
+bool insertItem (tItemL d, tPosL p, tList *L) {
+  tPosL q, r;
+
+  // Si no se ha podido crear un nuevo nodo de la lista,
+  // no se podrá insertar un nuevo elemento
+  if (!createNode(&q)) return false;
+
+  q -> data = d;
+  q -> next = LNULL;
+
+  if (*L == LNULL) *L = q; // Si la lista está vacía
+  else if (p == LNULL) // Si se quiere insertar al final
+}
--- programacionII/practica1/dynamic_list.h
+++ programacionII/practica1/dynamic_list.h
@@ -1,17 +1,51 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Miguel Taibo Pérez LOGIN 1: miguel.taibo.perez@udc.es
+ * AUTHOR 2: Marcos Estévez Besada LOGIN 2: marcos.estevez1@udc.es
+ * GROUP: 2.1
+ * DATE: ** / 02 / 26
  */
 
 #ifndef DYNAMIC_LIST_H
 #define DYNAMIC_LIST_H
 
+#define LNULL NULL
+
 #include "types.h"
 
-/* Write your code here... */
+typedef struct tNode* tPosL;
+typedef tPosL tList;
+
+typedef struct tNode {
+  iItemL data;
+  tPosL next;
+} tNode;
+
+// Este grupo de funciones sirven para crear una lista vacía
+// y para comprobar si una lista L está vacía o no.
+void createEmptyList (tList *L);
+bool isEmptyList (tList L);
+
+// Este grupo de funciones sirven para moverse por la lista,
+// obtener la posición siguiente, anterior, primera o última.
+tPosL first (tList L);
+tPosL last (tList L);
+tPosL next (tPosL p, tList L);
+tPosL previous (tPosL p, tList L);
+
+bool createNode(tPosL *P);
+
+// Este grupo de funciones modifican la lista, añadiendo,
+// eliminando o actualizando los elementos de la misma.
+bool insertItem (tItemL d, tPosL p, tList *L);
+void deleteAtPosition (tPosL p, tList *L);
+void updateItem (tItemL d, tPosL p, tList *L);
+
+// Este grupo de funciones buscan en la lista para obtener
+// un item concreto o la posición de un ítem concreto según
+// el projectName del mismo. No modifican las listas.
+tItemL getItem (tPosL p, tList L);
+tPosL findItem (tProjectName n, tList L);
 
 #endif

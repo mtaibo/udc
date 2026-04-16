65b79541b4c9f164b52c9bdaedcd5828d6e6001c
Miguel Taibo <dev@migueltaibo.com>
2026-04-16 11:35:48 +0200

    Declarada la estructura a seguir en el main, modificadas las funciones main, readTasks y processCommand para adecuarse a los requerimientos

--- programacionII/practica2/main.c
+++ programacionII/practica2/main.c
@@ -15,30 +15,61 @@
 #define MAX_BUFFER 255
 
 
+void create(tListC* list, char* committee, char* totalEvaluators) {}
+void new(tListC* list, char* committee, char* project, char* category) {}
+void stats(tListC* list) {}
+void vote(tListC* list, char* committee, char* project) {}
+void disqualify(tListC* list, char* project) {}
+void remove(tListC* list) {}
+void winners(tListC* list) {}
 
-void processCommand(char *commandNumber, char command, char *param1,char *param2, char *param3) {
+
+void processCommand(tListC* list, char *commandNumber, char command, char *param1,char *param2, char *param3) {
+
+    printf("********************\n");
 
     switch (command) {
         case 'C':
+            printf("%s %c: committee %s totalevaluators %s\n", commandNumber, command, param1, param2);
+            create(list, param1, param2);
             break;        
+
         case 'N':
+            printf("%s %c: committee %s project %s category %s\n", commandNumber, command, param1, param2, param3);
+            new(list, param1, param2, param3);
             break;
+
         case 'S':
+            printf("%s %c:\n", commandNumber, command);
+            stats(list);
             break;
+
         case 'V':
+            printf("%s %c: committee %s project %s\n", commandNumber, command, param1, param2);
+            vote(list, param1, param2);
             break;
+
         case 'D':
+            printf("%s %c: project %s\n", commandNumber, command, param1);
+            disqualify(list, param1);
             break;
+
         case 'R':
+            printf("%s %c:\n", commandNumber, command);
+            remove(list);
             break;
+
         case 'W':
+            printf("%s %c:\n", commandNumber, command);
+            winners(list);
             break;
+
         default:
             break;
     }
 }
 
-void readTasks(char *filename) {
+void readTasks(char *filename, tListC* list) {
     FILE *f = NULL;
     char *commandNumber, *command, *param1, *param2, *param3;
     const char delimiters[] = " \n\r";
@@ -55,7 +86,7 @@ void readTasks(char *filename) {
             param2 = strtok(NULL, delimiters);
             param3 = strtok(NULL, delimiters);
 
-            processCommand(commandNumber, command[0], param1, param2, param3);
+            processCommand(list, commandNumber, command[0], param1, param2, param3);
         }
 
         fclose(f);
@@ -70,6 +101,9 @@ int main(int nargs, char **args) {
 
     char *file_name = "create.txt";
 
+    tListC list; // Declaración de la varibale con una lista de comités
+    createEmptyListC(&list); // Inicialización en vacío de la lista
+
     if (nargs > 1) {
         file_name = args[1];
     } else {
@@ -78,7 +112,7 @@ int main(int nargs, char **args) {
         #endif
     }
 
-    readTasks(file_name);
+    readTasks(file_name, &list);
 
     return 0;
 }
